e9bcfba9d29bda794ccd07364c0dca0e98fb124b
Nathaniel Higgins <nat@nath.is>
2013-05-21 20:37:20 +0100

    Set a good title on error pages.

--- index.php
+++ index.php
@@ -98,6 +98,6 @@ foreach ($parts as $part) {
     $crumbs[ucwords($part)] = $path;
 }
 
-$title = array_last(array_keys($crumbs));
+$title = $error ? "Uh, oh" : array_last(array_keys($crumbs));
 
 require 'view.php';
